61cbb48a565fb1a6739b3fbe201175a94d41e95d
Tambet Matiisen <tambet.matiisen@gmail.com>
2015-12-03 13:59:26 +0200

    Fixed Linux scripts

--- CASIA_lfw_oversample.sh
+++ CASIA_lfw_oversample.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python src/kiosk.py --enlarge_factor=2.2 --image_size 100 --oversample 1 --images_path=images/lfw --mean_file=models/CASIA/CASIA_train_mean.binaryproto --data_file=data/CASIA_lfw/CASIA_lfw_oversample.csv --index_file=data/CASIA_lfw/CASIA_lfw_oversample.pkl --model_file=models/CASIA_lfw/CASIA_features.prototxt --pretrained_file=models/CASIA_lfw/CASIA_iter_450000.caffemodel --layer=pool5 --backend gpu $*
+python src/kiosk.py --enlarge_factor=2.2 --image_size 100 --oversample 1 --images_path=images/lfw --mean_file=models/CASIA/CASIA_train_mean.binaryproto --data_file=data/CASIA_lfw/CASIA_lfw_oversample.csv --index_file=data/CASIA_lfw/CASIA_lfw_oversample.pkl --model_file=models/CASIA/CASIA_features.prototxt --pretrained_file=models/CASIA/CASIA_iter_450000.caffemodel --layer=pool5 --backend gpu $*
